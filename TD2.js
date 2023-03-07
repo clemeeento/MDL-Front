a7addc04dbf4e435484ae1f6024cc41881c758c5
clemeeento <clement.furnon@me.com>
2023-03-07 12:16:00 +0100

    TD

--- TD2.js
+++ TD2.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 function main()
 {
     app.get("/",(req,res)=>{
-        const dt = new Date();
-        res.send("Hello ! " + dt);
-        console.log(dt);
+        res.sendFile(path.join("C:/Users/Administrateur/Documents/MethodeDeveloppementLogiciel/TDs/TD2/HTML","Home.html"));
     });
     app.listen(port, function(){ console.log("Serveur lancé sur le port " +port); });
 }
