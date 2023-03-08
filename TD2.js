cb67b51727eea7295663230d57d845131d5e9f3d
clemeeento <clement.furnon@me.com>
2023-03-08 12:01:31 +0100

    TD

--- TD2.js
+++ TD2.js
@@ -5,6 +5,13 @@ const port = 3000;
 function main()
 {
     app.use(express.static("Public"));
+
+    /*
+    app.get("/",(req,res)=>{
+        res.sendFile("/Public/Liste.html");
+    });
+    */
+   
     app.listen(port, function(){ console.log("Serveur lancé sur le port " +port); });
 }
 
