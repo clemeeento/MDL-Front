c67cbc2b4eaa7671b5ac101b47644ca0c8ace117
clemeeento <clement.furnon@me.com>
2023-03-20 17:22:26 +0100

    getJson API

--- Public/Script.js
+++ Public/Script.js
@@ -1,15 +1,21 @@
-function getData()
-{
-    const tableau = 
-    [{"id":1,"email":"isidro_von@hotmail.com","first":"Torrey","last":"Veum","company":"Google","created_at":"2014-12-25T04:06:27.981Z","country":"Switzerland"},
-        {"id":2,"email":"frederique19@gmail.com","first":"Micah","last":"Sanford","company":"Google","created_at":"2014-07-03T16:08:17.044Z","country":"Democratic People's Republic of Korea"},
-        {"id":3,"email":"fredy54@gmail.com","first":"Hollis","last":"Swift","company":"Microsoft","created_at":"2014-08-18T06:15:16.731Z","country":"Tunisia"},
-        {"id":4,"email":"braxton29@hotmail.com","first":"Perry","last":"Leffler","company":"Microsoft","created_at":"2014-07-10T11:31:40.235Z","country":"Chad"},
-        {"id":5,"email":"turner59@gmail.com","first":"Janelle","last":"Hagenes","company":"Amazon","created_at":"2014-04-21T15:05:43.229Z","country":"Swaziland"}];
-    return tableau;
+async function getAjaxResponse(url) {
+    return new Promise(function(resolve, reject) {
+        $.get(url, {}, function(data, status, xhr) {
+            if (status === "success") {
+                resolve(data);
+            } else {
+                reject(Error(xhr.statusText));
+            }
+        }, "json");
+    });
 }
+
+
 
 addEventListener("DOMContentLoaded",function () {
     console.log("Le document est chargé");
-    $("#client").loadTemplate($("#tmpClient"),getData());
+    const url = "https://localhost:3001api/customers";
+    const data =getAjaxResponse(url);
+    const users = JSON.parse(data);
+    $("#client").loadTemplate($("#tmpClient"),users);
 });
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const apiServ = require("./Presentation/apiPres");
+const express = require("express");
+const app = express();
 const port = 3000;
 
 function main()
 {
-    apiServ.start(port);
+    app.use(express.static("Public"));  
+
+    app.listen(port, function(){ console.log("Serveur lancé sur le port " +port); });
 }
 
 main();
+ 
