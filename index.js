a01a88784b6b8e0dbced3cd12af5bedf206287b9
clemeeento <clement.furnon@me.com>
2023-04-19 11:55:33 +0200

    API

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 function main()
 {
-    app.use(express.static("Public"));  
+    app.use(bodyParser.json());
+    app.use(cors());
 
+    app.use(express.static("Public"));  
+    app.use(cors());
     app.listen(port, function(){ console.log("Serveur lancé sur le port " +port); });
 }
 
