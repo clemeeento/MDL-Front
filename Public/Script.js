5cec5950e9b7462c039cc5a47d8dec891a5d50b5
clemeeento <clement.furnon@me.com>
2023-05-03 19:46:29 +0200

    API

--- Public/Script.js
+++ Public/Script.js
@@ -11,11 +11,51 @@ async function getAjaxResponse(url) {
 }
 
 
+document.addEventListener("DOMContentLoaded", async function() {
 
-addEventListener("DOMContentLoaded",async function () {
     console.log("Le document est chargé");
-    const url = "https://localhost:3001api/customers";
-    const data = await getAjaxResponse(url);
-    const users = JSON.parse(data);
-    $("#client").loadTemplate($("#tmpClient"),users);
+  
+    const url = "http://localhost:3001/api/customers";
+  
+    const resp = await getAjaxResponse(url);
+  
+    const customers = resp;
+  
+    const tableBody = document.getElementById("customers");
+    
+  
+    for (let i = 0; i < customers.length; i++) {
+      const customer = customers[i];
+  
+      const row = tableBody.insertRow();
+  
+      const idCell = row.insertCell();
+      idCell.innerHTML = customer.id;
+  
+      const lastCell = row.insertCell();
+      lastCell.innerHTML = customer.last;
+  
+      const firstCell = row.insertCell();
+      firstCell.innerHTML = customer.first;
+  
+      const companyCell = row.insertCell();
+      companyCell.innerHTML = customer.company;
+  
+      const countryCell = row.insertCell();
+      countryCell.innerHTML = customer.country;
+  
+      const emailCell = row.insertCell();
+      emailCell.innerHTML = customer.email;
+  
+      const createdAtCell = row.insertCell();
+      createdAtCell.innerHTML = customer.created_at;
+    }
 });
+
+// addEventListener("DOMContentLoaded",async function () {
+//     console.log("Le document est chargé");
+//     const url = "https://localhost:3001api/customers";
+//     const data = await getAjaxResponse(url);
+//     const users = JSON.parse(data);
+//     $("#client").loadTemplate($("#tmpClient"),users);
+// });
