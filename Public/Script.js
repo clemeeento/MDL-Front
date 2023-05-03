6c7c372c1e5dcd82a99a6e509c85fbf854d464fe
clemeeento <clement.furnon@me.com>
2023-05-03 19:40:16 +0200

    API

--- Public/Script.js
+++ Public/Script.js
@@ -1,21 +1,21 @@
-function getAjaxResponse(url) {
+async function getAjaxResponse(url) {
     return new Promise(function(resolve, reject) {
-        $.get(url, {}, function(data, status, xhr) {
-            if (status == "success") {
-                resolve(data);
-            } else {
-                reject(Error(xhr.statusText));
-            }
-        }, "json");
+      $.get(url, {}, function(data, status, xhr) {
+        if (status === "success") {
+          resolve(data);
+        } else {
+          reject(Error(xhr.statusText));
+        }
+      }, "json");
     });
 }
 
 
 
-addEventListener("DOMContentLoaded",function () {
+addEventListener("DOMContentLoaded",async function () {
     console.log("Le document est chargé");
     const url = "https://localhost:3001api/customers";
-    const data =getAjaxResponse(url);
+    const data = await getAjaxResponse(url);
     const users = JSON.parse(data);
     $("#client").loadTemplate($("#tmpClient"),users);
 });
