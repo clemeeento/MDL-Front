c7cd7653b918bf129117e1b4fe457e9bc2bbcfd7
clemeeento <clement.furnon@me.com>
2023-05-06 10:33:33 +0200

    Changement de page

--- Public/Script.js
+++ Public/Script.js
@@ -1,3 +1,6 @@
+let page = 1;
+const number = 15;
+
 function getAjaxResponse(number, page){
   return new Promise((resolve, reject) => {
       const urlApi = "http://localhost:3001/api/customers?number=" + number + "&page=" + page; 
@@ -14,41 +17,61 @@ function getAjaxResponse(number, page){
 async function table(number,page){
   const resp = await getAjaxResponse(number,page);
   
-    const customers = resp.result;
+  const customers = resp.result;
   
-    const tableBody = document.getElementById("customers");
+  const tableBody = document.getElementById("customers");
     
   
-    for (let i = 0; i < customers.length; i++) {
-      const customer = customers[i];
-  
-      const row = tableBody.insertRow();
-  
-      const idCell = row.insertCell();
-      idCell.innerHTML = customer.id;
-  
-      const lastCell = row.insertCell();
-      lastCell.innerHTML = customer.last;
-  
-      const firstCell = row.insertCell();
-      firstCell.innerHTML = customer.first;
-  
-      const companyCell = row.insertCell();
-      companyCell.innerHTML = customer.company;
-  
-      const countryCell = row.insertCell();
-      countryCell.innerHTML = customer.country;
-  
-      const emailCell = row.insertCell();
-      emailCell.innerHTML = customer.email;
-  
-      const createdAtCell = row.insertCell();
-      createdAtCell.innerHTML = customer.created_at;
-    }
+  for (let i = 0; i < customers.length; i++) {
+    const customer = customers[i];
+
+    const row = tableBody.insertRow();
+
+    // const idCell = row.insertCell();
+    // idCell.innerHTML = customer.id;
+
+    const lastCell = row.insertCell();
+    lastCell.innerHTML = customer.last;
+
+    const firstCell = row.insertCell();
+    firstCell.innerHTML = customer.first;
+
+    const companyCell = row.insertCell();
+    companyCell.innerHTML = customer.company;
+
+    const countryCell = row.insertCell();
+    countryCell.innerHTML = customer.country;
+
+    const emailCell = row.insertCell();
+    emailCell.innerHTML = customer.email;
+
+    // const createdAtCell = row.insertCell();
+    // createdAtCell.innerHTML = customer.created_at;
+  }
+}
+
+function clearTable() {
+  const tableBody = document.getElementById("customers");
+  while (tableBody.firstChild) {
+    tableBody.removeChild(tableBody.firstChild);
+  }
+}
+
+function pageSuivante(){
+  page=page+1;
+  clearTable();
+  table(number,page);
+}
+
+function pagePrecedente(){
+  page=page-1;
+  clearTable();
+  table(number,page);
 }
 
 
 document.addEventListener("DOMContentLoaded", async function() {
-  table(15,5);
+  
+  table(number,1);
     
 });
